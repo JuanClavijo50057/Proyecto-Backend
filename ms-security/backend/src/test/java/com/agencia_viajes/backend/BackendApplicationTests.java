3a4ae2702656eecaac1c375d59fce15869ae59da
JacoboArroyave <jacoboArroyave>
2025-08-24 20:25:37 -0500

    feat():Se inicializa el backend del micro servicio de seguridad

---
+++ ms-security/backend/src/test/java/com/agencia_viajes/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia_viajes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
